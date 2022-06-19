b0fa74f90d333c5915f7acd897162912dc19af9b
tokitsu <toki2222son@yahoo.co.jp>
2022-06-19 23:28:21 +0700

    add tag search

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
-Route::post('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('store');
-Route::get('/edit/{id}', [App\Http\Controllers\HomeController::class, 'edit'])->name('edit');
-Route::post('/update/{id}', [App\Http\Controllers\HomeController::class, 'update'])->name('update');
-Route::post('/delete/{id}', [App\Http\Controllers\HomeController::class, 'delete'])->name('delete');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
+    Route::post('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [App\Http\Controllers\HomeController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [App\Http\Controllers\HomeController::class, 'update'])->name('update');
+    Route::post('/delete/{id}', [App\Http\Controllers\HomeController::class, 'delete'])->name('delete');
+} );
