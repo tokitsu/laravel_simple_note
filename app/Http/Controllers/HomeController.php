c467549b81d08998fd5edd4a74c5df69db372cec
tokitsu <toki2222son@yahoo.co.jp>
2022-06-19 19:11:20 +0700

    add tag table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Memo;
+use App\Models\Tag;
 
 class HomeController extends Controller
 {
@@ -32,13 +33,22 @@ class HomeController extends Controller
     public function create()
     {
         $user = \Auth::user();
-        return view('create', compact('user'));
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('create', compact('user', 'memos'));
     }
+
 
     public function store(Request $request)
     {
         $data = $request->all();
-        Memo::insertGetId(['content' => $data['content'], 'user_id' => $data['user_id'], 'status' => 1]);
+
+        $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        if( empty($exist_tag['id']) ){
+            $tag_id = Tag::insertGetId(["name" => $data['tag'], 'user_id' => $data['user_id']]);
+        }else{
+            $tag_id = $exist_tag['id'];
+        };
+        Memo::insertGetId(['content' => $data['content'], 'user_id' => $data['user_id'], 'status' => 1, 'tag_id' => $tag_id]);
 
         return redirect()->route('home');
     }
@@ -47,14 +57,15 @@ class HomeController extends Controller
     {
         $user = \Auth::user();
         $memo = Memo::where('user_id', $user['id'])->where('id', $id)->where('status', 1)->first();
+        $tags = Tag::where('user_id', $user['id'])->get();
         $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
-        return view('edit', compact('memo', 'user', 'memos'));
+        return view('edit', compact('memo', 'user', 'memos' , 'tags'));
     }
 
     public function update(Request $request, $id)
     {
         $inputs = $request->all();
-        Memo::where('id', $id)->update(['content' => $inputs['content']]);
+        Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' => $inputs['tag_id']]);
         return redirect()->route('home');
     }
 
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,10 +12,15 @@
                 <div class="form-group">
                      <textarea name='content' class="form-control"rows="10">{{ $memo['content'] }}</textarea>
                 </div>
-                <!-- <div class="form-group">
-                    <label for="tag">タグ</label>
-                    <input name='tag' type="text" class="form-control" id="tag" placeholder="タグを入力">
-                </div> -->
+                
+                <div class="form-group">
+                    <select class='form-control' name='tag_id'>
+                @foreach($tags as $tag)
+                    <option value="{{ $tag['id'] }}" {{ $tag['id'] == $memo['tag_id'] ? "selected" : "" }}>{{$tag['name']}}</option>
+                @endforeach
+                    </select>
+                </div>
+                
                 <button type='submit' class="btn btn-primary btn-lg">更新</button>
             </form>
         </div>
